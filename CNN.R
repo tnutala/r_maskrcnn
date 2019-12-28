1189e43b3a41214e1b211d69b6a11a5ed7b9542c
Tejo Nutalapati <tnutala@gmail.com>
2019-12-27 23:22:43 -0600

    reading up on keras in r

--- CNN.R
+++ CNN.R
@@ -1,3 +1,19 @@
 library(keras)
 library(EBImage)
 
+
+# Defining the Model 
+
+model = keras_model_sequential()
+
+model %>%
+  layer_dense(units = 256, activation = "relu", input_shape = c(784)) %>%
+  layer_dropout(rate = 0.4) %>%
+  layer_dense(units = 128, activation = "relu") %>%
+  layer_dropout(rate = .3) %>%
+  layer_dense(units = 10, activation = "softmax")
+
+
+
+
+
